96083bfa5ea5c7d57edd239f188a2317c33015d3
nibor <116458882+pluiedorage76@users.noreply.github.com>
2023-06-21 10:51:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import os
 os.system("pip install importlib")
 os.system("pip install nltk")
+from nltk.corpus import words
+
+# Charger le dictionnaire anglais
+english_words = words.words()
+
+# Vérifier si un mot est dans le dictionnaire
+if 'apple' in english_words:
+    print("Le mot 'apple' est dans le dictionnaire anglais.")
